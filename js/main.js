f4ca7be4c3aa414a6cd5dfd21fc6030f690d31cd
Ivan Melnikov <klubauto.vk@gmail.com>
2022-06-09 20:27:40 +0300

    Добаляет функции для создания массива из сгенерированных объектов

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-// Ссылка на источник - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Массивы из доступных данных
+
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+// Количество генерируемых объектов
+
+const OBJECT_COUNT = 10;
+
+// Функция генерации случайных чисел. Ссылка на источник - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 const getRandomNumber = (min, max) => {
   if (min > max || min < 0) {
@@ -8,9 +23,7 @@ const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-getRandomNumber(1, 4);
-
-// Функция для Кексобукинга. Ссылка на источник - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+// Функция генерации случайных чисел с плавающей точкой. Ссылка на источник - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 
 const getRandomFractional = (min, max, digits = 5) => {
   if (min > max || min < 0) {
@@ -22,4 +35,59 @@ const getRandomFractional = (min, max, digits = 5) => {
   return Number(result);
 };
 
-getRandomFractional(1, 4);
+// Функция для получения случайного элемента из массива
+
+const getRandomArrayElement = (element) => element[getRandomNumber(0, element.length - 1)];
+
+// Функция для получения нескольких случайных элементов из массива
+
+const getRandomArrayElements = (elements) => {
+  const getShuffledArray = (elements).sort(()=>Math.random()-0.5); // Перемешивание массива
+  const getElements = getShuffledArray.slice(0, getRandomNumber(0, getShuffledArray.length)); // Получение случайных элементов из массива
+  const сonvertArrayToString = getElements.join(', '); // Преобразование массива в строку
+
+  return сonvertArrayToString;
+};
+
+// Функция для создания объекта (объявления)
+
+const createObject = (index) => {
+  const checkInOutTime = getRandomArrayElement(TIMES);
+
+  return {
+    author: {
+      avatar: `img/avatars/user${index < 10 ? `${'0'}${index + 1}` : index}.png`, // Тернарный оператор для подстановки нуля и увеличения числа на единицу
+    },
+    offer: {
+      title: 'Аренда недвижимости',
+      address: `${getRandomFractional(35.65000, 35.70000)}, ${getRandomFractional(139.70000, 139.80000)}`,
+      price: getRandomNumber(10000, 400000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomNumber(1, 8),
+      guests: getRandomNumber(1, 8),
+      checkin: checkInOutTime,
+      checkout: checkInOutTime,
+      features: getRandomArrayElements(FEATURES),
+      description: 'Лучшее предложение по доступной цене',
+      photos: getRandomArrayElements(PHOTOS),
+    },
+    location: {
+      lat: getRandomFractional(35.65000, 35.70000),
+      lng: getRandomFractional(139.70000, 139.80000),
+    }
+  };
+};
+
+// Функция для создания массива из объектов (объявлений)
+
+const generateOffers = (count) => {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    result.push(createObject(i));
+  }
+  return result;
+};
+
+generateOffers(OBJECT_COUNT);
+
+// console.log(generateOffers(OBJECT_COUNT));
