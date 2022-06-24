76978fdf034c4d36c96aa3953a7f68ba5de48704
Ivan Melnikov <klubauto.vk@gmail.com>
2022-06-24 15:43:49 +0300

    Создает модуль для генерации похожих объявлений

--- js/create-card.js
+++ js/create-card.js
@@ -1,16 +1,12 @@
-import {generateAds, FEATURES} from './data.js';
-import {getRandomArrayElements} from './util.js';
+import {generateAds} from './data.js';
 
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const mapCanvas = document.querySelector('#map-canvas');
 
-const randomFeatures = getRandomArrayElements(FEATURES);
-const containerFeatures = cardTemplate.querySelector('.popup__features');
-
-
 const photo = cardTemplate.querySelector('.popup__photo');
 
 const fragment = document.createDocumentFragment();
+const fragment2 = document.createDocumentFragment();
 
 const createAds = generateAds();
 
@@ -32,6 +28,19 @@ const getTranslate = (type) => {
   }
 };
 
+const createFeatureElement = (element, container) => {
+  element.forEach((item) => {
+    const featuresItem = container.querySelector(`.popup__feature--${item}`);
+    if (featuresItem) {
+      fragment2.append(featuresItem);
+    }
+  });
+
+  container.innerHTML = '';
+  container.append(fragment2);
+};
+
+
 cardTemplate.querySelector('.popup__photos').innerHTML = '';
 
 const createPhotoElement = (element, container) => {
@@ -44,19 +53,10 @@ const createPhotoElement = (element, container) => {
 
 const getSimilarAds = () => {
 
-  randomFeatures.forEach((item) => {
-    const featuresItem = containerFeatures.querySelector(`.popup__feature--${item}`);
-    if (featuresItem) {
-      fragment.append(featuresItem);
-    }
-  });
-
-  containerFeatures.innerHTML = '';
-  containerFeatures.append(fragment);
-
   createAds.forEach((element) => {
     const cardElement = cardTemplate.cloneNode(true);
     const containerPhotos = cardElement.querySelector('.popup__photos');
+    const containerFeatures = cardElement.querySelector('.popup__features');
 
     cardElement.querySelector('.popup__title').textContent = element.offer.title;
     cardElement.querySelector('.popup__text--address').textContent = element.offer.address;
@@ -64,6 +64,7 @@ const getSimilarAds = () => {
     cardElement.querySelector('.popup__type').textContent =  getTranslate(element.offer.type);
     cardElement.querySelector('.popup__text--capacity').textContent = `${element.offer.rooms} комнаты для ${element.offer.guests} гостей`;
     cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${element.offer.checkin}, выезд до ${element.offer.checkout}`;
+    createFeatureElement(element.offer.features, containerFeatures);
     cardElement.querySelector('.popup__description').textContent = element.offer.description;
     createPhotoElement(element.offer.photos, containerPhotos);
     cardElement.querySelector('.popup__avatar').src = element.author.avatar;
