97b485ee53154a5b1ebf72dbbe1091af4d0dc979
Ivan Melnikov <klubauto.vk@gmail.com>
2022-06-24 11:22:38 +0300

    Создает модуль для генерации похожих объявлений

--- js/create-card.js
+++ js/create-card.js
@@ -11,6 +11,8 @@ const photo = containerPhotos.querySelector('.popup__photo');
 const randomFeatures = getRandomArrayElements(FEATURES);
 const containerFeatures = cardTemplate.querySelector('.popup__features');
 
+const fragment = document.createDocumentFragment();
+
 const createAds = generateAds();
 
 const getTranslate = (type) => {
@@ -31,34 +33,40 @@ const getTranslate = (type) => {
   }
 };
 
-const featuresFragment = document.createDocumentFragment();
-randomFeatures.forEach((item) => {
-  const featuresItem = containerFeatures.querySelector(`.popup__feature--${item}`);
-  if (featuresItem) {
-    featuresFragment.append(featuresItem);
-  }
-});
-containerFeatures.innerHTML = '';
-containerFeatures.append(featuresFragment);
+const getSimilarAds = () => {
 
-randomPhotos.forEach((value) => {
-  photo.remove();
-  const clonePhoto = photo.cloneNode(true);
-  clonePhoto.src = value;
-  containerPhotos.append(clonePhoto);
-});
 
-createAds.forEach((element) => {
-  const cardElement = cardTemplate.cloneNode(true);
+  randomFeatures.forEach((item) => {
+    const featuresItem = containerFeatures.querySelector(`.popup__feature--${item}`);
+    if (featuresItem) {
+      fragment.append(featuresItem);
+    }
+  });
 
-  cardElement.querySelector('.popup__title').textContent = element.offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = element.offer.address;
-  cardElement.querySelector('.popup__text--price').textContent = `${element.offer.price} ₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent =  getTranslate(element.offer.type);
-  cardElement.querySelector('.popup__text--capacity').textContent = `${element.offer.rooms} комнаты для ${element.offer.guests} гостей`;
-  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${element.offer.checkin}, выезд до ${element.offer.checkout}`;
-  cardElement.querySelector('.popup__description').textContent = element.offer.description;
-  cardElement.querySelector('.popup__avatar').src = element.author.avatar;
+  containerFeatures.innerHTML = '';
+  containerFeatures.append(fragment);
 
-  mapCanvas.appendChild(cardElement);
-});
+  randomPhotos.forEach((value) => {
+    photo.remove();
+    const photoElement = photo.cloneNode(true);
+    photoElement.src = value;
+    containerPhotos.append(photoElement);
+  });
+
+  createAds.forEach((element) => {
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.popup__title').textContent = element.offer.title;
+    cardElement.querySelector('.popup__text--address').textContent = element.offer.address;
+    cardElement.querySelector('.popup__text--price').textContent = `${element.offer.price} ₽/ночь`;
+    cardElement.querySelector('.popup__type').textContent =  getTranslate(element.offer.type);
+    cardElement.querySelector('.popup__text--capacity').textContent = `${element.offer.rooms} комнаты для ${element.offer.guests} гостей`;
+    cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${element.offer.checkin}, выезд до ${element.offer.checkout}`;
+    cardElement.querySelector('.popup__description').textContent = element.offer.description;
+    cardElement.querySelector('.popup__avatar').src = element.author.avatar;
+    fragment.appendChild(cardElement);
+  });
+  mapCanvas.append(fragment);
+};
+
+export {getSimilarAds};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {generateAds, ADS_COUNT} from './data.js';
-import './create-card.js';
+import {getSimilarAds} from './create-card.js';
 
 generateAds(ADS_COUNT);
+
+getSimilarAds();
 
