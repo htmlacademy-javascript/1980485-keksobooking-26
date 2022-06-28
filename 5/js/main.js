6af582c9905cac0d84b37bf40e2f3910f29db62d
Keks <keks@htmlacademy.ru>
2022-06-28 09:44:01 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/main.js
+++ 5/js/main.js
@@ -3,6 +3,7 @@ import {renderCard} from './create-card.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 
-const card = renderCard(generateAds()[0]);
+const ads = generateAds();
+const card = renderCard(ads[0]);
 
 mapCanvas.append(card);
