466587dc3b5b229a48c1b58137c350a032c90d5b
Ivan Melnikov <klubauto.vk@gmail.com>
2022-06-28 09:39:03 +0300

    Делает правки

--- js/create-card.js
+++ js/create-card.js
@@ -3,8 +3,6 @@ import {getDeclensionWords} from './util.js';
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const photo = cardTemplate.querySelector('.popup__photo');
 
-const fragment = document.createDocumentFragment();
-
 const typeDictionary = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
@@ -13,63 +11,61 @@ const typeDictionary = {
   hotel: 'Отель'
 };
 
-const createFeatures = (element, container) => {
-  element.forEach((item) => {
+const declensionWordsMap = {
+  rooms: ['комната', 'комнаты', 'комнат'],
+  guests: ['гостя', 'гостей', 'гостей']
+};
+
+const createFeatures = (elements, container) => {
+  elements.forEach((item) => {
     const featuresItem = document.createElement('li');
-    featuresItem.classList.add('popup__feature');
-    featuresItem.classList.add(`popup__feature--${item}`);
+    featuresItem.classList.add('popup__feature', `popup__feature--${item}`);
     container.append(featuresItem);
   });
 };
 
-const createPhotos = (element, container) => {
-  element.forEach((value) => {
+const createPhotos = (elements, container) => {
+  elements.forEach((item) => {
     const photoElement = photo.cloneNode(true);
-    photoElement.src = value;
+    photoElement.src = item;
     container.append(photoElement);
   });
 };
 
-const renderCard = (offer) => {
+const renderCard = ({offer, author}) => {
+
+  const {
+    title,
+    address,
+    price,
+    type,
+    rooms,
+    guests,
+    checkin,
+    checkout,
+    features,
+    description,
+    photos
+  } = offer;
+
   const cardElement = cardTemplate.cloneNode(true);
   const containerPhotos = cardElement.querySelector('.popup__photos');
   const containerFeatures = cardElement.querySelector('.popup__features');
-
-  const {
-    author: {
-      avatar
-    },
-    offer: {
-      title,
-      address,
-      price,
-      type,
-      rooms,
-      guests,
-      checkin,
-      checkout,
-      features,
-      description,
-      photos
-    }
-  } = offer;
-
   cardElement.querySelector('.popup__title').textContent = title;
   cardElement.querySelector('.popup__text--address').textContent = address;
-  cardElement.querySelector('.popup__text--price').insertAdjacentHTML('afterbegin', price);
+  cardElement.querySelector('.popup__text--price').insertAdjacentHTML('afterbegin', `${price} `);
   cardElement.querySelector('.popup__type').textContent =  typeDictionary[type];
-  cardElement.querySelector('.popup__text--capacity').textContent = `${rooms} ${getDeclensionWords(rooms, ['комната', 'комнаты', 'комнат'])} для ${guests} ${getDeclensionWords(guests, ['гостя', 'гостей', 'гостей'])}`;
+  cardElement.querySelector('.popup__text--capacity').textContent = `${rooms} ${getDeclensionWords(rooms, declensionWordsMap.rooms)} для ${guests} ${getDeclensionWords(guests, declensionWordsMap.guests)}`;
   cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
-  const popupFeatures = cardElement.querySelector('.popup__features');
+  const popupDescription = cardElement.querySelector('.popup__description');
+  cardElement.querySelector('.popup__avatar').src = author.avatar;
 
   if (!features) {
-    popupFeatures.remove();
+    containerFeatures.remove();
   } else {
     containerFeatures.innerHTML = '';
     createFeatures(features, containerFeatures);
   }
-
-  const popupDescription = cardElement.querySelector('.popup__description');
 
   if (!description) {
     popupDescription.remove();
@@ -77,18 +73,12 @@ const renderCard = (offer) => {
     popupDescription.textContent = description;
   }
 
-  const popupPhotos =  cardElement.querySelector('.popup__photos');
-
   if (!photo) {
-    popupPhotos.remove();
+    containerPhotos.remove();
   } else {
     containerPhotos.innerHTML = '';
     createPhotos(photos, containerPhotos);
   }
-
-  cardElement.querySelector('.popup__avatar').src = avatar;
-
-  fragment.appendChild(cardElement);
 
   return cardElement;
 };
