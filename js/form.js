a512057fa3bbcd6813e5541dbe1155f9c1cc510c
Ivan Melnikov <klubauto.vk@gmail.com>
2022-06-29 08:55:20 +0300

    Меняет имя константы

--- js/form.js
+++ js/form.js
@@ -1,20 +1,20 @@
 const adForm = document.querySelector('.ad-form');
-const adFormFieldset = adForm.querySelectorAll('fieldset');
+const adFormFieldsets = adForm.querySelectorAll('fieldset');
 const mapFilters = document.querySelector('.map__filters');
-const mapFiltersFieldset = mapFilters.querySelectorAll('fieldset');
+const mapFiltersFieldsets = mapFilters.querySelectorAll('fieldset');
 
 const enablesInactiveMode = () => {
   adForm.classList.add('ad-form--disabled');
   mapFilters.classList.add('map__filters--disabled');
-  adFormFieldset.setAttribute('disabled', 'disabled');
-  mapFiltersFieldset.setAttribute('disabled', 'disabled');
+  adFormFieldsets.setAttribute('disabled', 'disabled');
+  mapFiltersFieldsets.setAttribute('disabled', 'disabled');
 };
 
 const enablesActiveMode = () => {
   adForm.classList.remove('ad-form--disabled');
   mapFilters.classList.remove('map__filters--disabled');
-  adFormFieldset.removeAttribute('disabled');
-  mapFiltersFieldset.removeAttribute('disabled');
+  adFormFieldsets.removeAttribute('disabled');
+  mapFiltersFieldsets.removeAttribute('disabled');
 };
 
 export {enablesInactiveMode, enablesActiveMode};
