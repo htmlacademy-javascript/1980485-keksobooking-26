9a81fc89f9ba524cb487b1c49d82ca1548a6b8bb
Ivan Melnikov <klubauto.vk@gmail.com>
2022-07-06 15:37:00 +0300

    Исправляет функцию с ошибкой вместимости комнат

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,9 +1,8 @@
 const capacityMap = {
   1: {value: ['1'], error: 'комната для 1 гостя'},
   2: {value: ['1', '2'], error: 'комнаты для 1 или 2 гостей'},
-  3: {value: ['1', '2', '3'], error: 'комнаты от 1 до 3 гостей'},
-  100: {value: ['0'], error: 'комнат не предназначены для гостей'},
-  getError: (room) => `${room} ${capacityMap[room].error}`
+  3: {value: ['1', '2', '3'], error: 'комнаты для 1, 2 или 3 гостей'},
+  100: {value: ['0'], error: 'комнат не предназначены для гостей'}
 };
 
 const MinPrice = {
@@ -24,17 +23,9 @@ const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
   errorTextParent: 'ad-form__element',
   errorTextClass: 'ad-form__error'
-});
+}, false);
 
-const getCapacityErrorMessage = () => {
-  if (+roomsNumber.value === +Object.keys(capacityMap)[roomsNumber.value] - 1) {
-    return capacityMap.getError(roomsNumber.value);
-  }
-
-  if (+roomsNumber.value === +Object.keys(capacityMap)[3]) {
-    return capacityMap.getError(roomsNumber.value);
-  }
-};
+const getErrorMessage = (roomCount) => `${roomCount} ${capacityMap[roomCount].error}`;
 
 const getPriceErrorMessage = () => {
   if (price.value < MinPrice[type.value]) {
@@ -45,8 +36,8 @@ const getPriceErrorMessage = () => {
 const  initValidation = () => {
   const validateCapacity = () => capacityMap[roomsNumber.value].value.includes(guestsNumber.value);
 
-  pristine.addValidator(guestsNumber, validateCapacity, getCapacityErrorMessage);
-  pristine.addValidator(roomsNumber, validateCapacity);
+  pristine.addValidator(guestsNumber, validateCapacity);
+  pristine.addValidator(roomsNumber, validateCapacity, getErrorMessage);
 
   const validatePrice = () => price.value >= MinPrice[type.value.toUpperCase()];
 
