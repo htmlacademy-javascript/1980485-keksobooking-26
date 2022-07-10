8973ede77d1a2339fdecf22cbc737a567a833855
Ivan Melnikov <klubauto.vk@gmail.com>
2022-07-10 22:20:56 +0300

    Исправляет ошибки в модуле slider

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,10 @@
-import {MinPrice} from './form-validate.js';
+import {Price} from './form-validate.js';
 
 const slider = document.querySelector('.ad-form__slider');
 const priceField = document.querySelector('#price');
 const typeField = document.querySelector('#type');
 
 const onTypeChange = () => {
-  priceField.value = MinPrice[typeField.value.toUpperCase()];
   slider.noUiSlider.set(priceField.value);
 };
 
@@ -13,9 +12,9 @@ const createSlider = () => {
   noUiSlider.create(slider, {
     range: {
       min: 0,
-      max: 100000,
+      max: Price.MAX,
     },
-    start: MinPrice[typeField.value.toUpperCase()],
+    start: Price.MIN[typeField.value],
     step: 1,
     connect: 'lower',
     format: {
