60d39fe47d73328da975bca1f3127ccae887255f
Ivan Melnikov <klubauto.vk@gmail.com>
2022-07-12 22:03:53 +0300

    Добавляет функцию показа ошибки

--- js/api.js
+++ js/api.js
@@ -1,8 +1,19 @@
+import {showAlert} from './util.js';
+
 const getData = (onSuccess) => {
-  fetch('https://26.javascript.pages.academy/keksobooking/data')
-    .then((response) => response.json())
+  fetch('https://26.javascript.pages.academ/keksobooking/data')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      showAlert('Ошибка загрузки данных. Попробуйте перезагрузить страницу.');
+    })
     .then((data) => {
       onSuccess(data);
+    })
+    .catch(() => {
+      showAlert('Ошибка загрузки данных. Попробуйте перезагрузить страницу.');
     });
 };
 
